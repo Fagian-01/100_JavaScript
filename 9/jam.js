0dd4d42da5fa57e7372d66998bf6e99d91670409
Fagian_Darmawan <fagian030709@gmail.com>
2025-06-30 13:19:00 +0700

    Delete 9 directory

--- 9/jam.js
+++
@@ -1,7 +0,0 @@
-function updateJam() {
-  let sekarang = new Date();
-  let jam = sekarang.getHours().toString().padStart(2, '0');
-  let menit = sekarang.getMinutes().toString().padStart(2, '0');
-  let detik = sekarang.getSeconds().toString().padStart(2, '0');
-  document.getElementById("jam").textContent = jam + ":" + menit + ":" + detik;
-}
