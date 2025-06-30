6d85c1d406cf3ad699221e287ff3a459d9ebaf6c
Fagian_Darmawan <fagian030709@gmail.com>
2025-06-30 13:18:53 +0700

    Delete 8 directory

--- 8/tabung.js
+++
@@ -1,5 +0,0 @@
-function hitung() {
-  let jari = parseFloat(document.getElementById("jari").value);
-  let luas = 2 * 3.14 * jari * (jari+1);
-  document.getElementById("hasil").textContent = "Luas: " + luas;
-}
