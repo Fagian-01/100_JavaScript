3d3b234ecdc3d0663a56d56b3f3e105198c79ec3
Fagian_Darmawan <fagian030709@gmail.com>
2025-06-30 13:18:24 +0700

    Delete 5 directory

--- 5/lingkaran.js
+++
@@ -1,5 +0,0 @@
-function hitung() {
-  let jari = parseFloat(document.getElementById("jari").value);
-  let luas = jari ** 2 * 3.14
-  document.getElementById("hasil").textContent = "Luas: " + luas;
-}
