f3e8b874d392fa9bbc80d19c5aefee8d2e15af81
Fagian_Darmawan <fagian030709@gmail.com>
2025-06-30 13:18:44 +0700

    Delete 7 directory

--- 7/persegi panjang.js
+++
@@ -1,6 +0,0 @@
-function hitung() {
-  let panjang = parseFloat(document.getElementById("panjang").value);
-  let lebar = parseFloat(document.getElementById("lebar").value);
-  let luas = panjang * lebar;
-  document.getElementById("hasil").textContent = "Luas: " + luas;
-}
